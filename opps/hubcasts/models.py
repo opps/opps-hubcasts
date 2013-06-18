fa8db6baece459e6edb8557f7c78e148aff406c1
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-18 17:51:55 -0300

    create basic models streaming

---
+++ opps/hubcasts/models.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from opps.core.models import Publishable
+
+
+class Streaming(Publishable):
+    type = models.CharField(_(u'Type'), max_length=1, choices=(
+        ('s', _(u'Shoutcast')), ('i', _(u'Icecast'))))
+    protocol = models.CharField(_(u'Protocol'), max_length=5, choices=(
+        ('http', _(u'HTTP')), ('https', _(u'HTTPS'))))
+    host = models.CharField(_(u'Host IPV4'), max_length=16)
+    port = models.PositiveIntegerField(verbose_name=_(u'Port TCP'),
+                                       max_length=8)
+
+    class Meta:
+        verbose_name = _(u'Streaming')
+        verbose_name_plural = _(u'Streaming')
