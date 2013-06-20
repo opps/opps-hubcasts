242956bc24fe3806d8e7884ed23f24279bbb4e63
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-20 17:03:26 -0300

    Create views for download streaming file

---
+++ opps/hubcasts/views.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mimetypes
+
+from django.views.generic.detail import DetailView
+
+from .models import Streaming
+
+
+class StreamingPlay(DetailView):
+    model = Streaming
+
+    def render_to_response(self, context, **response_kwargs):
+        self.ext = self.request.path.split('.')[1]
+
+        context['host'] = self.object.host
+        context['port'] = self.object.port
+        context['protocol'] = self.object.protocol
+        context['site'] = self.object.site
+
+        response_kwargs['content_type'] = mimetypes.guess_type(
+            'hubcasts/file.{}'.format(self.ext))[0]
+        return super(StreamingPlay, self).render_to_response(
+            context, mimetype=response_kwargs['content_type'],
+            **response_kwargs)
+
+    def get_template_names(self):
+        return ['hubcasts/file.{}'.format(self.ext)]
