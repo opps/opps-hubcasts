d666509738e53e137b95e2e44c097d301462cbef
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-20 15:38:35 -0300

    Add save model on admin

--- opps/hubcasts/admin.py
+++ opps/hubcasts/admin.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.contrib.auth import get_user_model
 from django.utils.translation import ugettext_lazy as _
 
 from .models import Streaming
@@ -16,6 +20,14 @@ class StreamingAdmin(admin.ModelAdmin):
             'classes': ('extrapretty'),
             'fields': ('published', 'date_available')}),
     )
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'pk', None) is None:
+            obj.user = get_user_model().objects.get(pk=request.user.pk)
+            obj.date_insert = timezone.now()
+            obj.site = Site.objects.get(pk=settings.SITE_ID)
+        obj.date_update = timezone.now()
+        obj.save()
 
 
 admin.site.register(Streaming, StreamingAdmin)
