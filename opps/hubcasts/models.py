b2b50175ec9da1908d7a7399d852790b5fa8956f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-12-20 13:14:18 -0200

    Add mms in Protocol

--- opps/hubcasts/models.py
+++ opps/hubcasts/models.py
@@ -10,19 +10,23 @@ class Streaming(Publishable):
     type = models.CharField(_(u'Type'), max_length=1, choices=(
         ('s', _(u'Shoutcast')), ('i', _(u'Icecast')), ('o', _(u'Other'))))
     protocol = models.CharField(_(u'Protocol'), max_length=5, choices=(
-        ('http', _(u'HTTP')), ('https', _(u'HTTPS')), ('rtmp', _(u'RTMP'))))
+        ('http', _(u'HTTP')),
+        ('https', _(u'HTTPS')),
+        ('rtmp', _(u'RTMP')),
+        ('mms', _(u'MMS')),
+    ),)
     host = models.CharField(_(u'Hostname'), max_length=50)
     port = models.PositiveIntegerField(verbose_name=_(u'Port TCP'),
                                        max_length=8, null=True, blank=True)
     sufix = models.PositiveIntegerField(verbose_name=_(u'Streaming sufix'),
-                                       max_length=25, null=True, blank=True)
+                                        max_length=25, null=True, blank=True)
 
     class Meta:
         verbose_name = _(u'Streaming')
         verbose_name_plural = _(u'Streamings')
 
     def __unicode__(self):
-    	return self.get_absolute_url()
+        return self.get_absolute_url()
 
     def get_absolute_url(self):
         if self.type == 'o':
